74812d1b34a526bec998f49eb668bd86b911ce6d
vaibhavisatange880 <126376109+vaibhavisatange880@users.noreply.github.com>
2024-06-22 17:06:51 +0530

    Transaction service implementation

---
+++ finance/src/main/java/com/example/finance/serviceimpl/TransactionService.java
@@ -0,0 +1,24 @@
+package com.example.finance.serviceimpl;
+
+import com.example.finance.entity.Transaction;
+import com.example.finance.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+}
+
