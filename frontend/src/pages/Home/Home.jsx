7d6a41cf2d67158a826000f77ac3d8878f1c0ec8
Sakshi <sakshisihag157@gmail.com>
2024-06-23 10:29:02 +0530

    designed the welcome page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,8 +5,26 @@ import Transactions from '../../components/Transactions/Transactions';
 import CreateTransaction from '../../components/createTransaction/createTransaction';
 
 function Home({ selectedContent }) {
+
+   const homeDefault = () => {
+    if (!selectedContent) {
+      return (
+        <div className='content'>
+          <h1>Welcome to Personal Finance Manager</h1>
+          <h2>Take control of your finances!</h2>
+          <p>Easily track expenses and manage income — all in one secure and intuitive platform. </p>
+          <p>Start your journey to financial freedom today!</p>
+          <p></p>
+        </div>
+      );
+    }
+    return null;
+  };
+
+
   return (
     <div className='home'>
+      {homeDefault()}
       {selectedContent === "profile" && <Profile />}
       {selectedContent === "prevTransactions" && <Transactions />}
       {selectedContent === "createTransaction" && <CreateTransaction />}
