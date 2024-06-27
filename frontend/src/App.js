664d6ddf9602cff08111c5574f274c9181b65452
Sakshi <sakshisihag157@gmail.com>
2024-06-27 14:06:56 +0530

    feature: homepage with navigation to login and registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
               <Route path="/home" element= { <Dashboard/>} />
               <Route path="/register" element= { <Register/>} />
               <Route path="/" element= { <Login/>} />
+              <Route path="/homepage" element= { <Home/>} />
             </Routes>
         </BrowserRouter>
       
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import './Home.css'
+
 function Home() {
     return (
-      <div>
-       <h1>Home</h1>
+      <div className="homepage">
+      <p className="fincare-text">FinCare</p>
+      <p className="message">Your Personal Finance Manager</p>
+      <div className="homepage-buttons">
+        <Link to="/" className="btn btn-primary">Login</Link>
+        <Link to="/register" className="btn btn-secondary">Register</Link>
       </div>
+    </div>
     );
   }
   
