238d8af9802ab39ea77af4cc5ba4fb1386be8c46
Sakshi <sakshisihag157@gmail.com>
2024-06-29 23:05:48 +0530

    modified the home page, deleted unnecess. files

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import './Home.css'
+import logo from '../util/piggy logo 3.png'
+
 
 function Home() {
     return (
-      <div className="homepage">
-      <p className="fincare-text">FinCare</p>
-      <p className="message">Your Personal Finance Manager</p>
-      <div className="homepage-buttons">
+      <>
+      <div className="navbar-container"> 
+        <img src={logo} alt="logo" />    
+        <p className="fincare-text">FinCare</p>
+      </div>  
+      <section className="main-content">
+          <div className="tag-lines">
+          <p className="tagline-1">Your Personal Finance Manager</p>
+          <p className="tagline-2">Track. Save. Thrive.</p>
+          <p className="tagline-3">Smart money management starts here!</p>
+          </div>
+        <div className="homepage-buttons">
         <Link to="/" className="btn btn-primary">Login</Link>
         <Link to="/register" className="btn btn-secondary">Register</Link>
-      </div>
-    </div>
+        </div>
+      </section>
+    </>
     );
   }
   
