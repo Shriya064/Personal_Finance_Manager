c1b25887a39875752c4b5d392ccbf65d7d04aae4
Sakshi <sakshisihag157@gmail.com>
2024-07-03 00:58:13 +0530

    modified the homepage, fixed login nd registration css, removed unused files & imports

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import './Home.css'
 import logo from '../util/piggy logo 3.png'
+import track from '../util/track-image.png';
+import bills from '../util/bills.png';
+import dashboard from '../util/dashboard-image.png';
 
 
 function Home() {
@@ -21,7 +24,56 @@ function Home() {
         <Link to="/" className="btn btn-primary">Login</Link>
         <Link to="/register" className="btn btn-secondary">Register</Link>
         </div>
+        <hr className="hr-element"/>
       </section>
+      
+      <section className="features-container">
+        <h2>Features</h2>
+        <div className="feature track">
+          <h3>Track your expenditures</h3>
+          <img src={track} alt="record-png" className="track-image" />
+        </div>
+        <div className="feature bills">
+          <img src={bills} alt="record-png" className="bills-image" />
+          <h3>Save your bills at one place</h3>
+        </div>
+        <div className="feature 1">
+          <h3>All-in-one Dashboard View</h3>
+          <img src={dashboard} alt="record-png" />
+        </div>
+      </section>
+      <hr className="hr-element"/>
+      <div className="teams-section">
+        <h2>Brought to you by</h2>
+        <h1>The Code Rangers</h1>
+        <div class="team-container">
+          <div class="team-member">
+            <img src="path/to/member1.jpg" alt="Member 1" class="team-photo"/>
+            <h3 class="team-name">Member 1</h3>
+            <p class="team-title">Title 1</p>
+          </div>
+          <div class="team-member">
+            <img src="path/to/member2.jpg" alt="Member 2" class="team-photo"/>
+            <h3 class="team-name">Member 2</h3>
+            <p class="team-title">Title 2</p>
+          </div>
+          <div class="team-member">
+            <img src="path/to/member3.jpg" alt="Member 3" class="team-photo"/>
+            <h3 class="team-name">Member 3</h3>
+            <p class="team-title">Title 3</p>
+          </div>
+          <div class="team-member">
+            <img src="path/to/member3.jpg" alt="Member 3" class="team-photo"/>
+            <h3 class="team-name">Member 4</h3>
+            <p class="team-title">Title 4</p>
+          </div>
+          <div class="team-member">
+            <img src="path/to/member3.jpg" alt="Member 3" class="team-photo"/>
+            <h3 class="team-name">Member 4</h3>
+            <p class="team-title">Title 4</p>
+          </div>
+      </div>
+      </div>
     </>
     );
   }
